8e696079c9f7bd7780f811636a833597c68b799a
wanttotakeacrap <dshkrsnnkv@mail.ru>
2024-10-05 01:33:21 +0300

    second commit

--- laba.py
+++
@@ -1,8 +0,0 @@
-class TV:
-    def __init__(self, name, price):
-        self.price = price
-        self.name = name
-class laptop:
-    def __init__(self, name, price):
-        self.price = price
-        self.name = name
